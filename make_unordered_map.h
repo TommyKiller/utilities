b85101348ddb23ce91382d697a3e77cdc39d24c7
Николай <Nicko6k9@yandex.ua>
2019-11-08 02:44:21 +0200

    Added custom hash handling

--- make_unordered_map.h
+++ make_unordered_map.h
@@ -5,34 +5,38 @@
 
 namespace details
 {
-	template <typename T1, typename T2, typename... Args>
+	template <typename T1, typename T2, typename THash, typename... Args>
 	struct map_types
 	{
 		using TPair = std::pair<T1, T2>;
 		using TKey = T1;
 		using TValue = T2;
+		using THasher = THash;
 	};
 
 	template <typename... Args>
-	struct map_types<void, void, Args...>
+	struct map_types<void, void, void, Args...>
 	{
 		using TPair = typename std::common_type<Args...>::type;
 		using TKey = typename std::tuple_element<0, TPair>::type;
 		using TValue = typename std::tuple_element<1, TPair>::type;
+		using THasher = typename std::hash<TKey>;
 	};
 }
 
-template <typename TKey = void, typename TValue = void, typename... Args>
+template <typename TKey = void, typename TValue = void, typename Hasher = void, typename... Args>
 std::unordered_map<
-	typename details::map_types<TKey, TValue, Args...>::TKey,
-	typename details::map_types<TKey, TValue, Args...>::TValue> make_unordered_map(Args&&... args)
+	typename details::map_types<TKey, TValue, Hasher, Args...>::TKey,
+	typename details::map_types<TKey, TValue, Hasher, Args...>::TValue,
+	typename details::map_types<TKey, TValue, Hasher, Args...>::THasher> make_unordered_map(Args&&... args)
 {
-	static_assert((std::is_convertible_v<Args, typename details::map_types<TKey, TValue, Args...>::TPair> &&...),
+	static_assert((std::is_convertible_v<Args, typename details::map_types<TKey, TValue, Hasher, Args...>::TPair> &&...),
 		"All arguments must be implicitly convertible to std::pair<TKey, TValue>");
 
 	std::unordered_map<
-		typename details::map_types<TKey, TValue, Args...>::TKey,
-		typename details::map_types<TKey, TValue, Args...>::TValue> map;
+		typename details::map_types<TKey, TValue, Hasher, Args...>::TKey,
+		typename details::map_types<TKey, TValue, Hasher, Args...>::TValue,
+		typename details::map_types<TKey, TValue, Hasher, Args...>::THasher> map;
 
 	map.reserve(sizeof...(Args));
 	(map.emplace(std::forward<Args>(args)), ...);
